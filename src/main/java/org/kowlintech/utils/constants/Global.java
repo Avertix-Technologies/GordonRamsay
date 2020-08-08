9dca6795b5317ad05b4e7f05420b8817682fc6a2
Kowlin Technologies <martingame665156@gmail.com>
2020-08-08 19:49:55 -0400

    Changed the feedback channel back to default.

--- src/main/java/org/kowlintech/utils/constants/Global.java
+++ src/main/java/org/kowlintech/utils/constants/Global.java
@@ -9,7 +9,7 @@ public class Global {
     // Important IDs
     public final static String SUPPORT = "683744919139254350";
 
-    public final static String FEEDBACK = "707024551661404250";
+    public final static String FEEDBACK = "683753359337062420";
     public final static String JOINLEAVE_LOGS = "683753870333181995";
     public final static String BOT_LOG = "683753919163400244";
 
