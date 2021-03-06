5a97037c889ac3eb4348db12674b548dd0eec02c
Kowlin Technologies <martingame665156@gmail.com>
2021-03-05 22:20:58 -0500

    Fixed a bug.

--- src/main/java/org/kowlintech/commands/misc/ShardInfoCommand.java
+++ src/main/java/org/kowlintech/commands/misc/ShardInfoCommand.java
@@ -24,20 +24,26 @@ public class ShardInfoCommand implements CommandExecutor {
             JDA.ShardInfo info = event.getJDA().getShardInfo();
             ShardManager manager = event.getJDA().getShardManager();
 
-            final long duration = (System.currentTimeMillis() - GordonRamsay.shard_start_times.get(info.getShardId()));
+            String uptime;
 
-            final long years = duration / 31104000000L;
-            final long months = duration / 2592000000L % 12;
-            final long days = duration / 86400000L % 30;
-            final long hours = duration / 3600000L % 24;
-            final long minutes = duration / 60000L % 60;
-            final long seconds = duration / 1000L % 60;
+            try {
+                final long duration = (System.currentTimeMillis() - GordonRamsay.shard_start_times.get(info.getShardId()));
 
-            String uptime = (years == 0 ? "" : "**" + years + "** Years, ") + (months == 0 ? "" : "**" + months + "** Months, ") + (days == 0 ? "" : "**" + days + "** Days, ") + (hours == 0 ? "" : "**" + hours + "** Hours, ")
-                    + (minutes == 0 ? "" : "**" + minutes + "** Minutes, ") + (seconds == 0 ? "" : "**" + seconds + "** Seconds, ");
+                final long years = duration / 31104000000L;
+                final long months = duration / 2592000000L % 12;
+                final long days = duration / 86400000L % 30;
+                final long hours = duration / 3600000L % 24;
+                final long minutes = duration / 60000L % 60;
+                final long seconds = duration / 1000L % 60;
 
-            uptime = replaceLast(uptime, ", ", "");
-            uptime = replaceLast(uptime, ",", " and");
+                uptime = (years == 0 ? "" : "**" + years + "** Years, ") + (months == 0 ? "" : "**" + months + "** Months, ") + (days == 0 ? "" : "**" + days + "** Days, ") + (hours == 0 ? "" : "**" + hours + "** Hours, ")
+                        + (minutes == 0 ? "" : "**" + minutes + "** Minutes, ") + (seconds == 0 ? "" : "**" + seconds + "** Seconds, ");
+
+                uptime = replaceLast(uptime, ", ", "");
+                uptime = replaceLast(uptime, ",", " and");
+            } catch (Exception ex) {
+                uptime = "";
+            }
 
             eb.setTitle("Shard Information");
             eb.setColor(Global.COLOR);
